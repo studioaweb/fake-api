41c95a226dcf1df56e9fac2f4d904256c57c931b
studioaweb <anderson@studioaweb.com.br>
2024-02-17 17:17:26 -0300

    Teste header

--- index.php
+++ index.php
@@ -1,5 +1,10 @@
 <?php
-header("Content-Type:application/json");
+header("Access-Control-Allow-Origin: *");
+header("Content-Type: application/json; charset=UTF-8");
+header("Access-Control-Allow-Methods: OPTIONS,GET,POST,PUT,DELETE");
+header("Access-Control-Max-Age: 3600");
+header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
+
 $uri_path = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
 $uri_segments = explode('/', $uri_path);
 $base_url = 'https://studioaweb.com.br/xstreaming/fake-api/';
