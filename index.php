39f100c90188e3d5ecf2b5edc34903d323ff28b3
studioaweb <anderson@studioaweb.com.br>
2024-02-17 17:13:40 -0300

    Teste json

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php
-
+header("Content-Type:application/json");
 $uri_path = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
 $uri_segments = explode('/', $uri_path);
 $base_url = 'https://studioaweb.com.br/xstreaming/fake-api/';
